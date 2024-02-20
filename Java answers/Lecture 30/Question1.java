4705430696cb8b697e9f1bdb99be8ccfc3b38129
Aryan Upadhyay <AryanUpadhyay1064@gmail.com>
2024-02-21 00:10:14 +0530

    a

--- Java answers/Lecture 30/Question1.java
+++ Java answers/Lecture 30/Question1.java
@@ -1,12 +1,5 @@
 Java provides support for printf style formatting of strings. Printf style formatting contains a format string and a number of different data type parameters. The format string contains plain text as well as format specifiers, which are special characters that format the parameters. The function produces an output string with parameters applied as per the format specifiers.
-
-For example:
-System.out.format("Fees = %f,  Age = %d and Name = %s", 12.95, 24, "Raj"); 
-produces an output of : Fees = 12.950000,  Age = 24 and Name = RajIn the above example,
-the format string is : "Fees = %f, Age = %d and Name = %s"
-the format specifiers are : %f, %d and %s
-the parameters are 12.95, 24 and "Raj"
-The format(...) method formatted the parameter values as per the format specifiers and produced the output string.
+formatted the parameter values as per the format specifiers and produced the output string.
 
 package q11185;
 public class StringFormatting {
--- Java answers/Lecture 33/Question4.java
+++ Java answers/Lecture 33/Question4.java
@@ -1,24 +1,5 @@
 Write a Java program to illustrate the single inheritance concept.
 
-Create a class Marks
-contains the data members id of int data type, javaMarks, cMarks and cppMarks of float data type
-write a method setMarks() to initialize the data members
-write a method displayMarks() which will display the given data
-Create another class Result which is derived from the class Marks
-contains the data members total and avg of float data type
-write a method compute() to find total and average of the given marks
-write a method showResult() which will display the total and avg marks
-Write a class SingleInheritanceDemo with main() method it receives four arguments as id, javaMarks, cMarks and cppMarks.
-
-Create object only to the class Result to access the methods.
-
-If the input is given as command line arguments to the main() as "101", "45.50", "67.75", "72.25" then the program should print the output as:
-Id : 101
-Java marks : 45.5
-C marks : 67.75
-Cpp marks : 72.25
-Total : 185.5
-Avg : 61.833332
 
 
 package q11263;
